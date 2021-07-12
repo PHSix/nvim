10b8d097782104d29683acfa9604235e375c4498
PH <15917977183@163.com>
2021-07-12 21:43:59 +0800

    update

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -6,67 +6,6 @@ local function telescope()
   vim.api.nvim_set_keymap("n", "<C-f>w", ":Telescope live_grep<CR>", {})
   vim.api.nvim_set_keymap("n", "<A-x>", ":Telescope commands<CR>", {})
   local actions = require("telescope.actions")
-  require("telescope").setup {
-    defaults = {
-      mappings = {
-        i = {
-          ["<esc>"] = actions.close,
-          ["<C-j>"] = actions.move_selection_next,
-          ["<C-k>"] = actions.move_selection_previous
-        }
-      },
-      vimgrep_arguments = {
-        "rg",
-        "--color=never",
-        "--no-heading",
-        "--with-filename",
-        "--line-number",
-        "--column",
-        "--smart-case"
-      },
-      prompt_position = "bottom",
-      prompt_prefix = " 🍭",
-      selection_caret = "> ",
-      entry_prefix = "  ",
-      initial_mode = "insert",
-      selection_strategy = "reset",
-      sorting_strategy = "descending",
-      layout_strategy = "horizontal",
-      layout_defaults = {
-        horizontal = {
-          mirror = false
-        },
-        vertical = {
-          mirror = false
-        }
-      },
-      file_sorter = require "telescope.sorters".get_fuzzy_file,
-      file_ignore_patterns = {},
-      generic_sorter = require "telescope.sorters".get_generic_fuzzy_sorter,
-      shorten_path = true,
-      winblend = 0,
-      width = 0.3,
-      preview_cutoff = 120,
-      results_height = 1,
-      results_width = 0.5,
-      border = {},
-      borderchars = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
-      color_devicons = true,
-      use_less = true,
-      set_env = {["COLORTERM"] = "truecolor"}, -- default = nil,
-      file_previewer = require "telescope.previewers".vim_buffer_cat.new,
-      grep_previewer = require "telescope.previewers".vim_buffer_vimgrep.new,
-      qflist_previewer = require "telescope.previewers".vim_buffer_qflist.new,
-      -- Developer configurations: Not meant for general override
-      buffer_previewer_maker = require "telescope.previewers".buffer_previewer_maker
-    },
-    extensions = {
-      fzy_native = {
-        override_generic_sorter = false,
-        override_file_sorter = true
-      }
-    }
-  }
 end
 
 return telescope
