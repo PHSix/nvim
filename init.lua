a4a7321d3c760e4793d8162f787c8972082b1668
PH <chenyi.ph@outlook.com>
2022-04-15 10:05:41 +0800

    perf: remove some unless code in init.lua

--- init.lua
+++ init.lua
@@ -7,25 +7,3 @@ function _G.complete_path(path)
 end
 
 require(vim.g.os .. ".core")
-local uv = vim.loop
-local opts = {
-	watch_entry = false, -- true = when dir, watch dir inode, not dir content
-	stat = false, -- true = don't use inotify/kqueue but periodic check, not implemented
-	recursive = false,
-}
-local ludash = require("ludash")
-local fn = function ()
-	print("write")
-end
-local rep 
-rep = function ()
-	local handle = uv.new_fs_event()
-	handle:start("/home/ph/.config/nvim/init.lua", opts, function ()
-		print("hello worl")
-		handle:stop()
-		handle:close()
-		fn()
-		rep()
-	end)
-end
-rep()
