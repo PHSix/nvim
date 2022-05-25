83db3a896dbb82f866726afe68e8607445460c5c
PH <chenyi.ph@outlook.com>
2022-05-25 13:29:14 +0800

    fix: fcitx5 setup

--- lua/ph/hack/fcitx5.lua
+++ lua/ph/hack/fcitx5.lua
@@ -1,35 +1,33 @@
 local M = {}
 local status = "1\n"
 local function set_chinese()
-  vim.fn.system("fcitx5-remote -o")
+	vim.fn.system("fcitx5-remote -o")
 end
 
 local function set_english()
-  vim.fn.system("fcitx5-remote -c")
+	vim.fn.system("fcitx5-remote -c")
 end
 
 function M.leave_insert()
-  status = vim.fn.system("fcitx5-remote")
-  if status == "1\n" then
-    return
-  end
-  set_english()
-  return
+	status = vim.fn.system("fcitx5-remote")
+	if status == "1\n" then
+		return
+	end
+	set_english()
 end
 
 function M.enter_insert()
-  if status == "1\n" then
-    return
-  end
-  set_chinese()
-  return
+	if status == "1\n" then
+		return
+	end
+	set_chinese()
 end
 
 function M.setup()
-  if vim.fn.executable("fcitx5") then
-    vim.cmd [[autocmd InsertLeave * lua require(_G.p("hack.fcitx5")).leave_insert()]]
-    vim.cmd [[autocmd InsertEnter * lua require(_G.p("hack.fcitx5")).enter_insert()]]
-  end
+	if vim.fn.executable("fcitx5") then
+		vim.cmd([[autocmd InsertLeave * lua require(_G.p("hack.fcitx5")).leave_insert()]])
+		vim.cmd([[autocmd InsertEnter * lua require(_G.p("hack.fcitx5")).enter_insert()]])
+	end
 end
 
 return M
--- lua/ph/hack/init.lua
+++ lua/ph/hack/init.lua
@@ -1,7 +1,10 @@
 local M = {}
+local health = require("_health")
 
 function M.setup()
-	require(_G.p("hack.fcitx5")).setup()
+	if health["fcitx5-remote"] then
+		require(_G.p("hack.fcitx5")).setup()
+	end
 	require(_G.p("hack.capslock")).setup()
 	require(_G.p("hack.autosave")).setup()
 end
