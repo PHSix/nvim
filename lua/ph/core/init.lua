8ab6006ab35146ae92a8e4d0649eb0370306485a
PH <chenyi.ph@outlook.com>
2022-05-31 21:33:32 +0800

    feat: enable dark for coolors

--- lua/ph/core/init.lua
+++ lua/ph/core/init.lua
@@ -4,7 +4,7 @@ local keymap = require(_G.p("core.keymap"))
 local autocmd = require(_G.p("core.autocmd"))
 local hack = require(_G.p("hack"))
 vim.g.python3_host_prog = "/bin/python3"
-require("coolors").debug("light")
+require("coolors").debug("dark")
 
 opts.setup()
 keymap.setup()
