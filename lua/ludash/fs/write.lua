4765dc2b1c99a8ae5c0bd6c7b051ca72f6b5601a
chenyi <chenyi.ph@outlook.com>
2022-06-08 13:58:50 +0800

    fix: right mode_t

--- lua/ludash/fs/write.lua
+++ lua/ludash/fs/write.lua
@@ -8,7 +8,7 @@ local uv = vim.loop
 -- @return {integer} write file status
 -- ]]
 local writeFileSync = function(file_path, content, flag)
-	local fd = assert(uv.fs_open(file_path, "w+", 438))
+	local fd = assert(uv.fs_open(file_path, "w+", 664))
 	local status = assert(uv.fs_write(fd, content))
 	assert(uv.fs_close(fd))
 	return status
--- test/writeFile-test.lua
+++ test/writeFile-test.lua
@@ -1,2 +1,3 @@
 local fs = require("ludash.fs")
-fs.writeFileSync("/home/ph/.config/nvim/temp.txt", "\thello\nworld\n\tme")
+-- fs.writeFileSync("/home/ph/.config/nvim/temp.txt", "\thello\nworld\n\tme")
+fs.writeFileSync("/Users/ph/")
