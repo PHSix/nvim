c3095b212286c2a847d85bdfa23f64d63e489fa6
chenyi <chenyi.ph@outlook.com>
2022-07-08 17:26:44 +0800

    guide indent plugin

--- lua/u/default.lua
+++ lua/u/default.lua
@@ -15,4 +15,17 @@ vim.defer_fn(function()
 	vim.g.vista_default_executive = "coc"
 
 	vim.g.exclude_ft = { "plaintext" }
+
+	require('indent_guides').setup({
+		indent_levels = 30;
+		indent_guide_size = 1;
+		indent_start_level = 1;
+		indent_enable = true;
+		indent_space_guides = true;
+		indent_tab_guides = false;
+		indent_soft_pattern = '\\s';
+		exclude_filetypes = { 'help', 'dashboard', 'dashpreview', 'NvimTree', 'vista', 'sagahover', "neo-tree" };
+		even_colors = { fg = '#2a3834', bg = '#332b36' };
+		odd_colors = { fg = '#332b36', bg = '#2a3834' };
+	})
 end, 1)
