be042c2228520612586bd17b7e37239bb8d1b57a
chenyi <chenyi.ph@outlook.com>
2022-10-02 10:55:39 +0800

    u

--- lua/u/plugins/indentline.lua
+++ lua/u/plugins/indentline.lua
@@ -40,9 +40,9 @@ vim.g.indent_blankline_context_patterns = {
 	"while",
 	"for",
 }
-vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
--- vim.opt.listchars:append("eol:↴")
+vim.o.list = true
+vim.opt.listchars:append("eol:↴,space: ")
+-- vim.opt.listchars:append("tab:|")
 
 require("indent_blankline").setup({
 	space_char_blankline = " ",
