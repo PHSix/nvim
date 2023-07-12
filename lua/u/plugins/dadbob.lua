a93c5ef3aab1d570400ca5f52e0720b0f0209228
ph <chenyi.ph@outlook.com>
2023-07-12 09:17:08 +0800

    u

--- lua/u/plugins/dadbob.lua
+++ lua/u/plugins/dadbob.lua
@@ -1,22 +1,25 @@
-local uv = vim.loop
+local uv = vim.uv or vim.loop
 local fn = vim.fn
-local PATH = '~/.dbs.json'
-uv.fs_open(fn.expand(PATH), "r", "438", function(err, fd)
-	if err == nil then
-		uv.fs_fstat(fd, function(err1, stat)
-			if err == nil then
-				uv.fs_read(fd, stat.size, 0, function(err, data)
-					if err == nil then
-						local ok, obj = pcall(vim.json.decode, data)
-						if ok then
-							vim.g.dbs = obj
-						end
-					end
-				end)
-			end
-		end
-		)
+local PATH = '~/.local_dbs_settings.json'
+
+uv.fs_open(fn.expand(PATH), "r", 438, function(err, fd)
+	if err ~= nil then
+		return
 	end
+	uv.fs_fstat(fd, function(err1, stat)
+		if err ~= nil then
+			return
+		end
+		uv.fs_read(fd, stat.size, 0, function(err, data)
+			if err ~= nil then
+				return
+			end
+			local ok, obj = pcall(vim.json.decode, data)
+			if ok then
+				vim.g.dbs = obj
+			end
+		end)
+	end)
 end)
 -- vim.cmd [[DBUIToggle]]
 
