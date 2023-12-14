ec35144565b35ef32a9be0b6437a557158ef2631
ph <chenyi.ph@outlook.com>
2023-12-14 13:43:32 +0800

    limit ts diable file size

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -10,6 +10,13 @@ function config.nvim_treesitter()
     highlight = {
       enable = true,
     },
+    disable = function(_, buf)
+      local max_filesize = 30 * 1024 -- 30 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
     textobjects = {
       select = {
         enable = true,
