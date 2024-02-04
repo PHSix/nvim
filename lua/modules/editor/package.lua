5e66cec0baa1f15e8607d6d498d8bfb8cb24e69f
ph <chenyi.ph@outlook.com>
2024-02-04 14:27:40 +0800

    add todo comment

--- lua/modules/editor/package.lua
+++ lua/modules/editor/package.lua
@@ -98,3 +98,13 @@ package({
   keys = '<C-t>',
 })
 
+package({
+  'folke/todo-comments.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+})
+
