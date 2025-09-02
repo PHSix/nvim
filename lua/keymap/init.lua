59ca62c04c8fb76559afc86b981c42c74a2d14b1
ph <chenyi.ph@outlook.com>
2025-09-02 22:04:05 +0800

    remove emacs keymap in insert mode

--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -62,13 +62,6 @@ cmap({
     { '<C-f>', '<Right>', opts(noremap) },
 })
 
-imap({
-    { '<C-f>', '<Right>', opts() },
-    { '<C-b>', '<Left>', opts() },
-    -- {'<C-n>', '<Down>', opts()},
-    -- {'<C-p>', '<Up>', opts()},
-})
-
 -- usage of plugins
 nmap({
     -- plugin manager: Lazy.nvim
